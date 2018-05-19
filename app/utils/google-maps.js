c72eac1c460fed1b768ab28697a40d8ffa42daa7
git-palace <edeveloper3@gmail.com>
2018-05-18 23:14:06 -0700

    add git utils

---
+++ app/utils/google-maps.js
@@ -0,0 +1,30 @@
+import EmberObject from '@ember/object';
+
+const google = window.google;
+
+export default EmberObject.extend({
+	init() {
+		this.set('geocoder', new window.google.maps(Geocoder()));
+	},
+
+	createMap(element, location) {
+		let map = new google.maps.Map(element, {
+			scrollWheel: false,
+			zoom: 10
+		});
+
+		this.pinLocation(location, map);
+		return map;
+	},
+
+	pinLocation(location, map) {
+		this.get('geocoder').geocode({address: location}, (result, status) => {
+			if (status === google.maps.GeocoderStatus.OK) {
+				let geometry = result[0].geometry.location;
+				let position = { lat: geometry.lat(), lng: geometry.lng() };
+				maps.setCenter(position);
+				new google.maps.Marker({ position, map, title: location });
+			}
+		});
+	}
+});
