4075491332b6a344e9d416f732d0234bed745824
git-palace <edeveloper3@gmail.com>
2018-05-18 21:25:03 -0700

    add async filtered results

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -1,19 +1,24 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-	classNames: ['list-filter'],
-	value: '',
+  classNames: ['list-filter'],
+  value: '',
 
-	init() {
-		this._super(...arguments);
-		this.get('filter')('').then( results => this.set('results', results) );
-	},
+  init() {
+    this._super(...arguments);
+    this.get('filter')('').then(allResults => this.set('results', allResults.results));
+  },
 
-	actions: {
-		handlerFilterEntry() {
-			let filterInputValue = this.get('value');
-			let filterAction = this.get('filter');
-			filterAction(filterInputValue).then( filteredResults => this.set('results', filteredResults) );
-		}
-	}
+  actions: {
+    handlerFilterEntry() {
+      let filterInputValue = this.get('value');
+      let filterAction = this.get('filter');
+
+      filterAction(filterInputValue)
+        .then(filteredResults => {
+          if (filteredResults.query === this.get('value'))
+            this.set('results', filteredResults.results);
+        });
+    }
+  }
 });
--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -3,10 +3,17 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 	actions: {
 		filterByCity(param) {
-			if ( param !== '' )
-				return this.get('store').query('rental', {'city': param});
+			if ( param !== '' ) {
+				return this.get('store')
+					.query('rental', {'city': param}).then(results => {
+						return { query: param, results: results };
+					});
+			}
 
-			return this.get('store').findAll('rental');
+			return this.get('store')
+				.findAll('rental').then(results => {
+					return { query: param, results: results };
+				});
 		}
 	}
 });
